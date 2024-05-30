26a3852c31b117e1c43858028b07e2fab53fe187
Branislav Pavkovic <branislav.pavkovic@librafire.com>
2024-05-30 15:54:50 +0200

    add hamburger menu

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,20 +1,34 @@
 // src/components/Navigation.js
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../style/Navigation.module.scss';
 
-const TopNavbar = () => {
+const Navigation = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <nav className={styles.nav}>
-            <div className={styles.logo}>
-              Petranaaaaaaa photography :D
-            </div>
-            <ul className={styles.menu}>
-                <li className={styles.menuItem}><a href="/">Home</a></li>
-                <li className={styles.menuItem}><a href="/about">About</a></li>
-                <li className={styles.menuItem}><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
+        <>
+            <nav className={styles.nav}>
+                <div className={styles.logo}>
+                    Petranaaaaaaa photography :D
+                </div>
+                <ul className={`${styles.menu} ${isOpen ? styles.open : ''}`}>
+                    <li className={styles.menuItem}><a href="/">Home</a></li>
+                    <li className={styles.menuItem}><a href="/about">About</a></li>
+                    <li className={styles.menuItem}><a href="/contact">Contact</a></li>
+                </ul>
+                <div className={styles.mobileMenuIcon} onClick={toggleMenu}>
+                    <div className={`${styles.icon} ${isOpen ? styles.open : ''}`}></div>
+                    <div className={`${styles.icon} ${isOpen ? styles.open : ''}`}></div>
+                    <div className={`${styles.icon} ${isOpen ? styles.open : ''}`}></div>
+                </div>
+            </nav>
+            <div className={`${styles.mobileBackdrop} ${isOpen ? styles.open : ''}`} onClick={toggleMenu}></div>
+        </>
     );
 };
 
-export default TopNavbar;
+export default Navigation;

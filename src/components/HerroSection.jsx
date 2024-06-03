5ea413d09b8dd3b5036fcbbe9f57f41034105eea
Zeljko Vidakovic <vidakovic.zeljko@zahoo.com>
2024-06-03 21:23:24 +0200

    Promenio naslovne slike

--- src/components/HerroSection.jsx
+++ src/components/HerroSection.jsx
@@ -1,15 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import styles from '../style/HeroSection.module.scss';
+import React, { useState, useEffect } from "react";
+import styles from "../style/HeroSection.module.scss";
 
-import Image from '../slike/NASLOVNA.jpg';
-import Image1 from '../slike/9M0A1888.jpg';
-import Image2 from '../slike/9M0A1889.jpg';
+import Image from "../slike/NASLOVNA.jpg";
+import Image2 from "../slike/9M0A5415-2.jpg";
+import Image1 from "../slike/5T3A5386.jpg";
 
-const images = [
-  Image,
-  Image1,
-  Image2
-];
+const images = [Image, Image2, Image1];
 
 const HeroSection = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -34,18 +30,25 @@ const HeroSection = () => {
           key={index}
           src={image}
           alt={`Background ${index}`}
-          className={`${styles.image} ${currentImageIndex === index ? styles.visible : ''} ${fade ? styles.fadeIn : styles.fadeOut}`}
+          className={`${styles.image} ${
+            currentImageIndex === index ? styles.visible : ""
+          } ${fade ? styles.fadeIn : styles.fadeOut}`}
           style={{ zIndex: currentImageIndex === index ? 1 : 0 }}
         />
       ))}
       <div className={styles.overlay}></div>
       <div className={styles.content}>
-        <p className={styles.subtitle}>Uđite u svet gde se svaki trenutak pretvara u umetnost a svaki kadar nosi deo srca fotografa.</p>
+        <p className={styles.subtitle}>
+          Uđite u svet gde se svaki trenutak pretvara u umetnost a svaki kadar
+          nosi deo srca fotografa.
+        </p>
         <div className={styles.indicators}>
           {images.map((_, index) => (
             <div
               key={index}
-              className={`${styles.indicator} ${currentImageIndex === index ? styles.active : ''}`}
+              className={`${styles.indicator} ${
+                currentImageIndex === index ? styles.active : ""
+              }`}
             />
           ))}
         </div>
